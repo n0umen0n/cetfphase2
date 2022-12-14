03a81b36f48f8306c3cac26ee63733bc6f53b4f5
n0umen0n <46645013+n0umen0n@users.noreply.github.com>
2022-12-14 12:50:22 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "compiling..."
-eosio-cpp -abigen -I ./include -contract cetf -o cetf.wasm src/cetf.cpp 
+cdt-cpp -abigen -I ./include -contract cetf -o cetf.wasm src/cetf.cpp 
 shasum -a 256 cetf.wasm
 
 
